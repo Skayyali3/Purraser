7ae598f0cc0bd963e69857317d65635f247fc0c1
Saif Kayyali <saifkayyali2011@gmail.com>
2026-07-11 13:30:01 +0300

    Code update: Servos insha'Allah controlled via bluetooth, waiting on KY-008

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -7,32 +7,47 @@ Servo base;
 Servo shoulder;
 Servo elbow;
 
-const int basePotentio = A0;
-const int shoulderPotentio = A1;
-const int elbowPotentio = A2;
+char data;
+
+int baseVal = 90;
+int shoulderVal = 90;
+int elbowVal = 90;
+
+const int MAX_ANGLE = 170;
+const int MIN_ANGLE = 10;
 
 void setup() {
+  BTSerial.begin(9600);
+  Serial.begin(9600);
+
   base.attach(7);
   shoulder.attach(6);
   elbow.attach(5);
-  pinMode(basePotentio, INPUT);
-  pinMode(shoulderPotentio, INPUT);
-  pinMode(elbowPotentio, INPUT);
-  BTSerial.begin(9600);
-  Serial.begin(9600);
+
+  base.write(baseVal);
+  shoulder.write(shoulderVal);
+  elbow.write(elbowVal);
 }
 
 void loop() {
-  int baseValue = analogRead(basePotentio);
-  baseValue = map(baseValue, 0, 1023, 0, 180);
-  base.write(baseValue);
-  delay(50);
-  int shoulderValue = analogRead(shoulderPotentio);
-  shoulderValue = map(shoulderValue, 0, 1023, 0, 180);
-  shoulder.write(shoulderValue);
-  delay(50);
-  int elbowValue = analogRead(elbowPotentio);
-  elbowValue = map(elbowValue, 0, 1023, 0, 180);
-  elbow.write(elbowValue);
+  if (BTSerial.available() > 0) {
+
+    data = BTSerial.read();
+    Serial.print("Received data: "); Serial.println(data);
+
+    if (data == 'L' && baseVal < MAX_ANGLE) baseVal += 5;
+    if (data == 'R' && baseVal > MIN_ANGLE) baseVal -= 5;
+
+    if (data == 'F' && shoulderVal < MAX_ANGLE) shoulderVal += 5;
+    if (data == 'B' && shoulderVal > MIN_ANGLE) shoulderVal -= 5;
+
+    if (data == 'U' && elbowVal < MAX_ANGLE) elbowVal += 5;
+    if (data == 'D' && elbowVal > MIN_ANGLE) elbowVal -= 5;
+  }
+
+  base.write(baseVal);
+  shoulder.write(shoulderVal);
+  elbow.write(elbowVal);
+
   delay(50);
 }
