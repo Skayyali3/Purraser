4da87c34570597a32f9fbc298fee5ebd122370a0
Saif Kayyali <saifkayyali2011@gmail.com>
2026-07-16 16:03:42 +0300

    Added code for laser + all characters processed as lowercase

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -16,6 +16,8 @@ int elbowVal = 90;
 const int MAX_ANGLE = 170;
 const int MIN_ANGLE = 10;
 
+const int laserPin = 12;
+
 void setup() {
   BTSerial.begin(9600);
   Serial.begin(9600);
@@ -27,21 +29,29 @@ void setup() {
   base.write(baseVal);
   shoulder.write(shoulderVal);
   elbow.write(elbowVal);
+
+  pinMode(laserPin, OUTPUT);
 }
 
 void loop() {
   if (BTSerial.available() > 0) {
     data = BTSerial.read();
+
+    data = tolower(data);
+    
     Serial.print("Received data: "); Serial.println(data);
 
-    if (data == 'L' && baseVal < MAX_ANGLE) baseVal += 5;
-    if (data == 'R' && baseVal > MIN_ANGLE) baseVal -= 5;
+    if (data == 'l' && baseVal < MAX_ANGLE) baseVal += 5;
+    if (data == 'r' && baseVal > MIN_ANGLE) baseVal -= 5;
 
-    if (data == 'F' && shoulderVal < MAX_ANGLE) shoulderVal += 5;
-    if (data == 'B' && shoulderVal > MIN_ANGLE) shoulderVal -= 5;
+    if (data == 'f' && shoulderVal < MAX_ANGLE) shoulderVal += 5;
+    if (data == 'b' && shoulderVal > MIN_ANGLE) shoulderVal -= 5;
 
-    if (data == 'U' && elbowVal < MAX_ANGLE) elbowVal += 5;
-    if (data == 'D' && elbowVal > MIN_ANGLE) elbowVal -= 5;
+    if (data == 'u' && elbowVal < MAX_ANGLE) elbowVal += 5;
+    if (data == 'd' && elbowVal > MIN_ANGLE) elbowVal -= 5;
+
+    if (data == 'i') digitalWrite(laserPin, 1); // cuz its the closest letter in terms of shape to binary ON
+    if (data == 'o') digitalWrite(laserPin, 0); //same reason as above except binary OFF
 
     base.write(baseVal);
     shoulder.write(shoulderVal);
